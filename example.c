52274346dea63aae36aa9e536db86c0178fda5ad
Leo Singer <leo.singer@ligo.org>
2024-05-01 22:50:33 -0400

    First commit

---
+++ example.c
@@ -0,0 +1,21 @@
+#include <Python.h>
+
+static PyObject *hello() {
+    return PyUnicode_FromString("Hello, world");
+}
+
+static PyMethodDef methods[] = {
+    {"hello", hello, METH_NOARGS, NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+static PyModuleDef moduledef = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "example",
+    .m_methods = &methods
+};
+
+PyMODINIT_FUNC PyInit_example(void)
+{
+    return PyModule_Create(&moduledef);
+}
